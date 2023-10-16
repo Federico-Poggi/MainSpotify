791d0cd23a63f3555e57f514ce9d8763baa1c935
federicopoggi <138136326+federicopoggi@users.noreply.github.com>
2023-10-16 13:30:18 +0200

    js-scaletta

---
+++ homeSpotify/home.js
@@ -0,0 +1,15 @@
+// Caricare dati con una API Request con relativi album img e dati autore
+// Dati relativi a album artista
+// Al play va anche fatta partire la traccia
+// Aggiungere search params per permettere all'utente di cercare le canzoni
+
+const spotifyAPI = "";
+const album = "";
+
+const albumIntoPage = () => {};
+
+// EXTRA
+// Aggiungere login or sign in
+// Aggiungere filtrare per genere o autore
+// Aggiungere una sezione preferiti
+// Permettere la creazione di playlist personali
