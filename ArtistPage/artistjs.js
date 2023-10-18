daff68e6d019cf8f8cc979b40f6e9bddfd5122d7
Federico Poggi <federicopoggi3@gmail.com>
2023-10-19 00:10:38 +0200

    card andPageJsDaFinire

--- ArtistPage/artistjs.js
+++ ArtistPage/artistjs.js
@@ -27,19 +27,19 @@ fetch(
       console.log(content);
 
       const art = (artistSection.innerHTML = `<li
-        class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
+        class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0 '
       >
         <div class='d-flex align-items-center'>
           <h5 class="px-3">1-</h5>
 
-          <img src="${content.data[i].album.cover_small}" alt="img"/>
+          <img  src="${content.data[i].album.cover_small}" alt="img"/>
         </div>
 
-        <div class='px-2'>${content.data[i].title}</div>
+        <div class='px-2 title'>${content.data[i].title}</div>
 
         <div></div>
 
-        <div class='px-2'>${content.data[i].duration}</div>
+        <div class='px-2 duration'>${content.data[i].duration}</div>
       </li>
       <li
         class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
@@ -50,11 +50,11 @@ fetch(
           <img src="${content.data[i + 3].album.cover_small}" alt="img"/>
         </div>
 
-        <div class='px-2'>${content.data[i + 3].title}</div>
+        <div class='px-2 title'>${content.data[i + 3].title}</div>
 
         <div></div>
 
-        <div class='px-2'>${content.data[i + 3].duration}</div>
+        <div class='px-2 duration'>${content.data[i + 3].duration}</div>
       </li>
       <li
         class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
@@ -62,14 +62,14 @@ fetch(
         <div class='d-flex align-items-center'>
           <h5 class="px-3">3-</h5>
 
-          <img src="${content.data[i + 5].album.cover_small}" alt="img"/>
+          <img  src="${content.data[i + 5].album.cover_small}" alt="img"/>
         </div>
 
-        <div class='px-2'>${content.data[i + 5].title}</div>
+        <div class='px-2 title'>${content.data[i + 5].title}</div>
 
         <div></div>
 
-        <div class='px-2'>${content.data[i + 5].duration}</div>
+        <div class='px-2 duration'>${content.data[i + 5].duration}</div>
       </li>
       <li
         class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
@@ -77,14 +77,14 @@ fetch(
         <div class='d-flex align-items-center'>
           <h5 class="px-3">4-</h5>
 
-          <img src="${content.data[i + 7].album.cover_small}" alt="img"/>
+          <img  src="${content.data[i + 7].album.cover_small}" alt="img"/>
         </div>
 
-        <div class='px-2'>${content.data[i + 7].title}</div>
+        <div class='px-2 title' >${content.data[i + 7].title}</div>
 
         <div></div>
 
-        <div class='px-2'>${content.data[i + 7].duration}</div>
+        <div class='px-2 duration'>${content.data[i + 7].duration}</div>
       </li>
       <li
         class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
@@ -92,14 +92,14 @@ fetch(
         <div class='d-flex align-items-center'>
           <h5 class="px-3">5-</h5>
 
-          <img src="${content.data[i + 9].album.cover_small}" alt="img"/>
+          <img  src="${content.data[i + 9].album.cover_small}" alt="img"/>
         </div>
 
-        <div class='px-2'>${content.data[i + 9].title}</div>
+        <div class='px-2 title'>${content.data[i + 9].title}</div>
 
         <div></div>
 
-        <div class='px-2'>${content.data[i + 9].duration}</div>
+        <div class='px-2 duration'>${content.data[i + 9].duration}</div>
       </li>`);
 
       // artistSection.appendChild(art);
