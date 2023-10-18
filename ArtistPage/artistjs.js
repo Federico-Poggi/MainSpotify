7d37a155437ab9a6b916ab71d5855cacc7754f1c
federicopoggi <138136326+federicopoggi@users.noreply.github.com>
2023-10-18 16:55:33 +0200

    Merge branch 'Album-page' into artistpage

--- ArtistPage/artistjs.js
+++ ArtistPage/artistjs.js
@@ -3,96 +3,49 @@
 const btnBar = document.getElementById("search");
 const query = localStorage.getItem("query");
 const artistSection = document.getElementById("section");
-// const artistSong = () => {
-//   // const searchWrap = document.getElementById("search");
-
-//   // cosi ho ottenuto un htmlCollection
-//   fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
-//     .then((res) => {
-//       if (res.ok) {
-//         return res.json();
-//       }
-
-//       console.log(res);
-//     })
-
-//     .then((content) => {
-//       console.log(content);
-//       artistSection.innerText = ``;
-//       for (let i = 0; i < content.data.length; i++) {
-//         console.log(content.data);
-
-//         artistSection.innerHTML = `<li
-//         class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
-//       >
-//         <div class='d-flex align-items-center'>
-//           <h5></h5>
-
-//           <img src="${content.data[i].album.cover_small}" alt="img"/>
-//         </div>
-
-//         <div class='px-2'></div>
-
-//         <div></div>
-
-//         <div class='px-2'>4:35</div>
-//       </li>`;
-
-//         artistSection.appendChild(art);
-//         console.log(artistSection);
-//       }
-//     })
-//     .catch((err) => {
-//       console.log("error", err);
-//     });
-// };
-const artistPage = (query) => {
-  fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then((content) => {
-      for (let i = 0; content[5]; i++) {
-        const album = content[i].data;
-        console.log(album);
-      }
-    });
-};
+const searchName = query;
+console.log(query);
 
 // const searchWrap = document.getElementById("search");
-// const searchValue = searchWrap.value;
-// console.log(artistCard);
-// console.log(searchWrap);
-// // cosi ho ottenuto un htmlCollection
 
-// fetch(
-//   `https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchValue}`
-// ).then((res) => {
-//   if (res.ok) {
-//     return res.json();
-//   }
-// });
+// cosi ho ottenuto un htmlCollection
+fetch(
+  `https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchName}`
+)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Error", err);
+    }
+  })
 
-// const insetrLi = () => {
-//   artistCard.innerHTML = `<li
-//   class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
-// >
-//   <div class='d-flex align-items-center'>
-//     <h5>1</h5>
+  .then((content) => {
+    console.log(content);
+    artistSection.innerHTML = ``;
+    for (let i = 0; i < content.data.length; i++) {
+      console.log(content);
 
-//     <img src='' alt= />
-//   </div>
+      const art = (artistSection.innerHTML = `<li
+        class = 'list-group-item d-flex align-items-center bg-transparent border-0 text-white px-0'
+      >
+        <div class='d-flex align-items-center'>
+          <h5></h5>
 
-//   <div class='px-2'></div>
+          <img src="${content.data[i].album.cover_small}" alt="img"/>
+        </div>
 
-//   <div></div>
+        <div class='px-2'></div>
 
-//   <div class='px-2'>4:35</div>
-// </li>`;
+        <div></div>
 
-//   console.log(artistCard);
-// };
+        <div class='px-2'>4:35</div>
+      </li>`);
 
-// insetrLi();
+      artistSection.appendChild(art);
+      console.log(artistSection);
+    }
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
