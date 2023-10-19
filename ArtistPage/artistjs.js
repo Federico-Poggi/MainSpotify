7cc5f9d3ff0ecf99b5b31c223c2d1af757e045f8
Federico Poggi <federicopoggi3@gmail.com>
2023-10-19 13:27:04 +0200

    firstPart semiComplete

--- ArtistPage/artistjs.js
+++ ArtistPage/artistjs.js
@@ -4,13 +4,17 @@ const sectionPopular = document.getElementById("contentpopular");
 const btnBar = document.getElementById("search");
 const query = localStorage.getItem("query");
 const artistSection = document.getElementById("section");
-
+const divSmall = document.getElementsByClassName("scrollmenu")[0];
+const artistName = document.getElementById("header");
+const artistTitle = document.getElementById("nameArtist");
+const listener = document.getElementById("listener");
 const searchName = query;
 console.log(query);
 
 // const searchWrap = document.getElementById("search");
 
 // cosi ho ottenuto un htmlCollection
+
 fetch(
   `https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchName}`
 )
@@ -24,6 +28,9 @@ fetch(
 
   .then((content) => {
     console.log(content);
+    artistTitle.innerText = content.data[0].artist.name;
+    listener.innerText = content.data[0].rank;
+    console.log(listener);
     artistSection.innerHTML = ``;
     let counter = 0;
     for (let i = 0; i < 5; i++) {
@@ -50,31 +57,27 @@ fetch(
       const art = document.createElement("li");
       art.className =
         "list-group, d-flex , align-items-center , bg-transparent , border-0 , text-white , mt-3 , px-0 ";
-      art.innerHTML = `<div class='d-flex align-items-center'>
-          <h5 class="px-3 text-light">${counter}-</h5>
+      art.innerHTML = `<div class=' d-flex align-items-center'>
+          <h5  class="px-3 text-light counter  m-0">${counter}</h5>
 
-          <img  src="${content.data[i].album.cover_small}" alt="img"/>
+          <img  class="w-50" src="${
+            content.data[i].album.cover_small
+          }" alt="img"/>
         </div>
 
-        <p class='px-2 title m-0 text-light w-20'>${
+        <p  class=' title m-0 text-light w-20' style="padding-left: 2%">${
           content.data[i].title_short
         }</p>
 
-        <span class='px-2 m-0 text-light w-20'>${content.data[i].rank}</span>
+        <span class='px-2 m-0  w-20 rank'>${content.data[i].rank}</span>
 
-        <p class='px-2 duration text-light'>${toMinute(time)}</p>`;
+        <p class='px-2 duration '>${toMinute(time)}</p>`;
       artistSection.appendChild(art);
     }
     return content.data;
   })
 
   .then((dates) => {
-    // const addressBarContent = new URLSearchParams(location.search);
-    // const myAlbum = addressBarContent.get(`album`);
-
-    const addressBarContent = new URLSearchParams(location.search);
-    const myAlbum = addressBarContent.get(`album`);
-    console.log();
     for (let j = 0; j < 7; j++) {
       fetch(
         `https://striveschool-api.herokuapp.com/api/deezer/album/${dates[j].album.id}`
@@ -90,15 +93,26 @@ fetch(
         .then((dataAlbum) => {
           console.log(dataAlbum);
           const sectionDiv = document.createElement("div");
-          sectionDiv.className = "card , mx-0 bg-transparent , border-0 , w-10";
+          // const divSmall = document.createElement("div");
+          // divSmall.className = "d-flex , scrollmenu";
+          sectionDiv.className =
+            "card , mx-0 bg-transparent , border-0 , w-10 , remove ";
+
           sectionDiv.innerHTML = `<img class="card-img" src="${dates[j].album.cover_medium}" alt="" />
     
           <div class="card-body p-0 text-center">
-            <h4 class="h4 text-light fs-7">${dataAlbum.title}</h4>
+            <h4 class="h4 text-light fs-7 sezione">${dataAlbum.title}</h4>
             <p class="h6 text-light fs-8">${dataAlbum.release_date} - Album</p>
           </div>`;
-
           sectionPopular.appendChild(sectionDiv);
+
+          const imgRel = document.createElement("img");
+          imgRel.className = "px-2 w-40";
+
+          imgRel.setAttribute("src", dates[j].album.cover_medium);
+          imgRel.setAttribute("alt", "img");
+          console.log(imgRel);
+          divSmall.appendChild(imgRel);
         })
 
         .catch((err) => {
